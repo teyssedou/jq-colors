e6e3a1cd248ebcfe5288a140d1f31faf25e0ddb8
teyssedou <mikaelteyssedou@gmail.com>
2017-12-08 15:40:48 +0100

    exo fait

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// good luck !
+$(".color").each(function(){
+	var color = $(this).attr("data-color");
+	$(this).css("background-color", color)
+	
+	$(this).click(function(){
+		if ($("#modify-texte").is(":checked")){
+			$("p").css("color", color)
+		}
+		else{
+			$(".main").css("background-color", color)
+		}
+	})
+
+
+})
